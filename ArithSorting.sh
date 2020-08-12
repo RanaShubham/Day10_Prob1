a4066df44407e4a390214b443370a055313f16fc
Shubham Rana <ranashubham7296@gmail.com>
2020-08-12 12:56:35 +0530

    Refactoring compute_UC5 to add all calculations in dictionary named calc

--- ArithSorting.sh
+++ ArithSorting.sh
@@ -2,16 +2,18 @@
 
 echo "Welcome to Arithmetic computation and sorting Program"
 
-declare -a Numbers
+declare -A calc
 
 read -p "Enter a number: " a
 read -p "Enter another number: " b
 read -p "Enter one last number: " c
 
-awk "BEGIN {print ${a}+${b}*$c}"
+calc["calc1"]=`awk "BEGIN {print ${a}+${b}*$c}"`
 
-awk "BEGIN {print $a*$b+$c}"
+calc["calc2"]=`awk "BEGIN {print $a*$b+$c}"`
 
-awk "BEGIN {print $c + $a/$b}"
+calc["calc3"]=`awk "BEGIN {print $c + $a/$b}"`
 
-awk "BEGIN {print $a%$b + $c}"
+calc["calc4"]=`awk "BEGIN {print $a%$b + $c}"`
+
+echo "${calc[@]}"
