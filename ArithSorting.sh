f2db4b9536668b925dfe31b7fc9d6a8345e9d2f8
Shubham Rana <ranashubham7296@gmail.com>
2020-08-12 18:32:32 +0530

    Refactoring desSortArr_UC8 to add ascending order algorithm for array

--- ArithSorting.sh
+++ ArithSorting.sh
@@ -48,4 +48,30 @@ do
 
 done
 
+k=o
+
 echo "Sorted_descending: ${calcArr[@]}"
+
+for (( i=0; i<(${#calcArr[@]}-1); i++ ))
+do
+
+        min=$i
+        for (( j=($i+1); j<${#calcArr[@]}; j++ ))
+        do
+                flag=`awk "BEGIN { print ( ${calcArr[$j]} < ${calcArr[$min]} )? "1" : "0" }"`
+
+                if [ $flag -eq 1 ]
+                then
+                        min=$j
+                fi
+        done
+
+        temp="${calcArr[$k]}"
+        calcArr[k]="${calcArr[$min]}"
+        calcArr[$min]=$temp
+        ((k++))
+
+done
+
+echo "Sorted_ascending: ${calcArr[@]}"
+	
