07eeea253a9b6716a03df468b7d779fb75fd3e1e
Shubham Rana <ranashubham7296@gmail.com>
2020-08-13 16:57:29 +0530

    Refactoring master branch to take 3 number as input

--- ArithSorting.sh
+++ ArithSorting.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
 
-echo "Welcome to Arithmetic computation and sorting Program."
+echo "Welcome to Arithmetic computation and sorting Program"
+
+declare -a Numbers
+
+read -p "Enter a number: " a
+read -p "Enter another number: " b
+read -p "Enter one last number: " c
